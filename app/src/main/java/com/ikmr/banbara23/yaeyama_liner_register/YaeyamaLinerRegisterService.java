cc181a03b18ee6d707aefe5c523e4fb909e2ceaf
池村 和剛 <kazutakai@tt-com.net>
2016-06-07 07:13:06 +0900

    #2 パース結果を送信

--- app/src/main/java/com/ikmr/banbara23/yaeyama_liner_register/YaeyamaLinerRegisterService.java
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_register/YaeyamaLinerRegisterService.java
@@ -60,16 +60,24 @@ public class YaeyamaLinerRegisterService extends BasePeriodicService {
             public void run() {
                 try {
                     Weather weather = WeatherParser.pars("http://weather.yahoo.co.jp/weather/jp/47/9410.html");
-                    String weatherJson = new Gson().toJson(weather);
+                    final String weatherJson = new Gson().toJson(weather);
+                    final String key = getString(R.string.pref_weather_key);
+                    // 前回の値と比較
+                    if (isEqualForLastTime(weatherJson, key)) {
+                        // 前回の結果と同じ値ならAPI送信しない
+                        Log.d("YaeyamaLinerRegisterSer", "前回と同じ天気");
+                        return;
+                    }
+
                     NCMBObject obj = new NCMBObject("Weather");
                     obj.put("weather", weatherJson);
-
                     obj.saveInBackground(new DoneCallback() {
                         @Override
                         public void done(NCMBException e) {
                             if (e == null) {
                                 // 保存成功
                                 Log.d("MainActivity", "weatherJson 送信成功");
+                                PreferenceUtils.put(key, weatherJson);
                             } else {
                                 // 保存失敗
                                 Log.d("MainActivity", "weatherJson 送信失敗 :" + e);
@@ -82,6 +90,14 @@ public class YaeyamaLinerRegisterService extends BasePeriodicService {
             }
         });
         thread.start();
+    }
+
+    private boolean isEqualForLastTime(String json, String key) {
+        String lastTimeString = PreferenceUtils.get(key, "");
+        if (lastTimeString.equals(json)) {
+            return true;
+        }
+        return false;
     }
 
     @Override
