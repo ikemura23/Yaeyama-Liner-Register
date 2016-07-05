13e56f9da16ada11c56ae4d48f40dec8f6132b84
banbara <ikemura@example.com>
2016-07-05 16:11:25 +0900

    #9 詳細パースをRxJavaにとりあえず埋め込む

--- app/src/main/java/com/ikmr/banbara23/yaeyama_liner_register/AnneiDetailController.java
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_register/AnneiDetailController.java
@@ -2,7 +2,6 @@ package com.ikmr.banbara23.yaeyama_liner_register;
 
 import android.util.Log;
 
-import com.ikmr.banbara23.yaeyama_liner_register.annei.AnneiDetailApiClient;
 import com.ikmr.banbara23.yaeyama_liner_register.annei.AnneiDetailParser;
 import com.ikmr.banbara23.yaeyama_liner_register.annei.AnneiParsHelper;
 import com.ikmr.banbara23.yaeyama_liner_register.entity.Port;
@@ -12,40 +11,41 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import rx.Observable;
 import rx.Subscriber;
+import rx.functions.Action1;
 import rx.functions.Func1;
 import rx.schedulers.Schedulers;
 
 public class AnneiDetailController {
     public static void start() {
-        List<Port> portList = AnneiParsHelper.getTargetPortList();
         Observable
-                .from(portList)
-                .create(new Observable.OnSubscribe<Document>() {
+                .create(new Observable.OnSubscribe< List<ResultDetail>>() {
                     @Override
-                    public void call(Subscriber<? super Document> subscriber) {
+                    public void call(Subscriber<? super  List<ResultDetail>> subscriber) {
                         Document document;
                         try {
                             String url = Base.getResources().getString(R.string.url_annei_list);
                             document = Jsoup.connect(url).timeout(Const.CONNECTION_TIME_OUT).get();
-                            subscriber.onNext(document);
-                            subscriber.onCompleted();
+
                         } catch (IOException e) {
                             subscriber.onError(e);
+                            return;
                         }
+                        List<Port> portList = AnneiParsHelper.getTargetPortList();
+                        List<ResultDetail> resultDetailList = new ArrayList<>();
+                        for (Port port : portList) {
+                            resultDetailList.add(AnneiDetailParser.pars(document, port));
+                        }
+                        subscriber.onNext(resultDetailList);
+                        subscriber.onCompleted();
                     }
                 })
                 .subscribeOn(Schedulers.newThread())
-                .map(new Func1<Document, ResultDetail>() {
-                    @Override
-                    public ResultDetail call(Document document) {
-                        return AnneiDetailParser.pars(document, port);
-                    }
-                })
-                .subscribe(new Subscriber<ResultDetail>() {
+                .subscribe(new Subscriber< List<ResultDetail>>() {
                     @Override
                     public void onCompleted() {
                         Log.i("AnneiDetailController", "onCompleted");
@@ -57,8 +57,8 @@ public class AnneiDetailController {
                     }
 
                     @Override
-                    public void onNext(ResultDetail resultDetail) {
-                        Log.i("AnneiDetailController", "onNext " + resultDetail.toString());
+                    public void onNext( List<ResultDetail> resultDetailList) {
+                        Log.i("AnneiDetailController", "onNext " + resultDetailList.toString());
                     }
                 });
     }
