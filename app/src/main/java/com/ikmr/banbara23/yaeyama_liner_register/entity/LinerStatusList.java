6bd386a2fb9404c45685f75d45bdb0f94828f67e
池村 和剛 <kazutakai@tt-com.net>
2016-07-21 18:55:31 +0900

    #21 create class LinerStatusList

---
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_register/entity/LinerStatusList.java
@@ -0,0 +1,15 @@
+package com.ikmr.banbara23.yaeyama_liner_register.entity;
+
+import java.util.List;
+
+public class LinerStatusList {
+    List<LinerStatus> linerStatusList;
+
+    public List<LinerStatus> getLinerStatusList() {
+        return linerStatusList;
+    }
+
+    public void setLinerStatusList(List<LinerStatus> linerStatusList) {
+        this.linerStatusList = linerStatusList;
+    }
+}
