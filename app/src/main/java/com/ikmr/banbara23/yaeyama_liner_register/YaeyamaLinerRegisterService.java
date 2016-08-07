258bc7f284b888cbcb78b8e2b3034286c37b6ae9
池村 和剛 <kazutakai@tt-com.net>
2016-08-08 06:43:56 +0900

    #28 デバッグ後片付け

--- app/src/main/java/com/ikmr/banbara23/yaeyama_liner_register/YaeyamaLinerRegisterService.java
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_register/YaeyamaLinerRegisterService.java
@@ -8,7 +8,10 @@ import com.ikmr.banbara23.yaeyama_liner_register.annei.AnneiStatusListApi;
 import com.ikmr.banbara23.yaeyama_liner_register.dream.DreamStatusListApi;
 import com.ikmr.banbara23.yaeyama_liner_register.entity.LinerStatusList;
 import com.ikmr.banbara23.yaeyama_liner_register.entity.Result;
+import com.ikmr.banbara23.yaeyama_liner_register.html.HtmlController;
 import com.ikmr.banbara23.yaeyama_liner_register.util.PreferenceUtils;
+import com.ikmr.banbara23.yaeyama_liner_register.weather.WeatherController;
+import com.ikmr.banbara23.yaeyama_liner_register.ykf.YkfController;
 import com.nifty.cloud.mb.core.DoneCallback;
 import com.nifty.cloud.mb.core.NCMBException;
 import com.nifty.cloud.mb.core.NCMBObject;
@@ -38,11 +41,11 @@ public class YaeyamaLinerRegisterService extends BasePeriodicService {
 
         try {
             KLog.d("execTask");
-//            startAnneiListQuery();
-//            YkfController.start();
+            startAnneiListQuery();
+            YkfController.start();
             startDreamListQuery();
-//            WeatherController.start();
-//            HtmlController.start();
+            WeatherController.start();
+            HtmlController.start();
         } catch (Exception e) {
             KLog.d("YaeyamaLinerRegisterSer", e.getMessage());
         }
