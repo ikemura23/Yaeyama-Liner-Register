3fb784e44a03fa07ea7f481f679dcefb5351be19
池村 和剛 <kazutakai@tt-com.net>
2016-12-20 23:39:18 +0900

    #40 トップ情報に運休を認識しないバグ修正

--- app/src/main/java/com/ikmr/banbara23/yaeyama_liner_register/top/TopController.java
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_register/top/TopController.java
@@ -89,10 +89,11 @@ public class TopController {
     private Status getStatus(List<Liner> liners) {
         for (Liner liner : liners) {
             if (liner.getStatus() == Status.CANCEL) {
-                return Status.CANCEL;   // １件でも欠航があればその会社は「欠航あり」
-            }
-            if (liner.getStatus() == Status.CAUTION) {
-                return Status.CAUTION;   // １件でも欠航があればその会社は「未定あり」
+                return Status.CANCEL;    // １件でも欠航があればその会社は「欠航あり」
+            } else if (liner.getStatus() == Status.SUSPEND) {
+                return Status.SUSPEND;   // １件でも運休があればその会社は「未定あり」
+            } else if (liner.getStatus() == Status.CAUTION) {
+                return Status.CAUTION;   // １件でも未定があればその会社は「未定あり」
             }
         }
         return Status.NORMAL;   // 通常運航
